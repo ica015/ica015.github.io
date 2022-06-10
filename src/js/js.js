dfeb8c88095e6827275e6e1eda273006723979e0
Carlos Henrique <ica015@hotmail.com>
2022-06-10 14:27:33 -0300

    Inclusão de menu responsivo para dispositivos móveis

---
+++ src/js/js.js
@@ -0,0 +1,11 @@
+function abre_menu(){
+    document.getElementById("menu").style.transform = "translateX(0px)";
+    document.getElementById("ativa_menu").style.display = "none";
+    document.querySelector("section").classList.add('opaco')
+}
+
+function fecha_menu(){
+    document.getElementById("menu").style.transform = "translateX(-300px)";
+    document.getElementById("ativa_menu").style.display = "block";
+    document.querySelector("section").classList.remove('opaco')
+}
