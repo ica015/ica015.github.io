add4ed88b057a397fa89198b321862c547112d7e
Carlos Henrique <ica015@hotmail.com>
2022-09-23 12:11:26 -0300

    Inclusão de chat para envio de mensagens e retirada do menu de 'contato', por não ser mais necessário

--- src/js/js.js
+++ src/js/js.js
@@ -13,6 +13,21 @@ window.onload = function(){
             }
         }
     })
+
+    // Typebot
+    var typebotCommands = Typebot.initBubble({
+        url: "https://viewer.typebot.io/ica015",
+        proactiveMessage: {
+        avatarUrl: "https://avatars.githubusercontent.com/u/37461891?v=4",
+        textContent: "Precisa de ajuda?",
+        delay: 1000,
+        },
+        button: {
+        //   iconUrl: "https://www.creativefabrica.com/wp-content/uploads/2019/04/Chat-icon-by-ahlangraphic-24.jpg",
+        color: "#888",
+        },
+    });
+
 }
 
 window.onscroll = () =>{
